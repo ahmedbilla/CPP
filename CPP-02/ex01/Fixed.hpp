4fe36de6c2f65f474f935ac7812f6b821556d92c
Ahmed billa <keygen@localhost.localdomain>
2025-11-04 18:09:11 +0100

    cpp02

--- CPP-02/ex01/Fixed.hpp
+++ CPP-02/ex01/Fixed.hpp
@@ -14,22 +14,35 @@
 #define FIXED_HPP
 
 #include <iostream>
- 
+#include <cmath>
+
 class Fixed
 {
-    private:
-        int value;
-        static const int fractionalBits = 8;
-    public:
-        Fixed();
-        Fixed(const int number) : value(number << fractionalBits)
-        Fixed (const float number) : value(number << fractionalBits)
+private:
+    int value;
+    static const int fractionalBits = 8;
+
+public:
+    Fixed();
+    Fixed(const int number) : value(number << fractionalBits)
+    {
+    }
+    Fixed(const float number) : value(roundf(number * (1 << fractionalBits)))
+    {
+    }
+    float Fixed::toFloat(void) const
+    {
+        return (float)this->value / (1 << fractionalBits);
+    }
+     int toInt( void ) const
+     {
         
-        Fixed(const Fixed &other);
-        Fixed &operator=(const Fixed &op);
-        int getRawBits( void ) const;
-        void setRawBits( int const raw);
-        ~Fixed();
+     }
+    // Fixed(const Fixed &other);
+    // Fixed &operator=(const Fixed &op);
+    // int getRawBits( void ) const;
+    // void setRawBits( int const raw);
+    ~Fixed();
 };
 
 #endif
