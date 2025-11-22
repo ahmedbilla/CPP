7b7da1d0cdc46bcd0dbd330a2f33327e4810b61e
Ahmed Billa <ahbilla@e1r1p3.1337.ma>
2025-11-22 13:30:02 +0100

    CPP-03/ex01 : finish

--- CPP-03/ex01/ScavTrap.cpp
+++ CPP-03/ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: ahbilla <ahbilla@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 23:56:23 by ahbilla           #+#    #+#             */
-/*   Updated: 2025/11/22 00:07:42 by ahbilla          ###   ########.fr       */
+/*   Updated: 2025/11/22 13:29:00 by ahbilla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,42 @@ void ScavTrap::attack(const std::string &target)
         std::cout << "ScavTrap " << Name << "can't attack because it has no hit points or energy left!" << std::endl;
         return;
     }
+}
+
+ScavTrap::ScavTrap() : ClapTrap()
+{
+    HitPoint = 100;
+    EnergyPoint = 50;
+    AttackDamage = 20;
+    std::cout << Name << " ScavTrap constructor is called" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string parm) : ClapTrap(parm)
+{
+    HitPoint = 100;
+    EnergyPoint = 50;
+    AttackDamage = 20;
+    std::cout << Name << " ScavTrap parametrize constructor is called" << std::endl;
+}
+
+void ScavTrap::guardGate()
+{
+    std::cout << Name << " is in Gate Keeper mode" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &other) : ClapTrap(other)
+{
+    std::cout << Name << " ScavTrap copy constructor called" << std::endl;
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &other)
+{
+    (ClapTrap::operator=(other));
+    std::cout << Name << " ScavTrap copy assignment operator called" << std::endl;
+    return *this;
+}
+
+ScavTrap::~ScavTrap()
+{
+    std::cout << Name << " ScavTrap destructor is called" << std::endl;
 }
--- CPP-03/ex01/ScavTrap.hpp
+++ CPP-03/ex01/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: ahbilla <ahbilla@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 10:13:36 by ahbilla           #+#    #+#             */
-/*   Updated: 2025/11/22 12:29:43 by ahbilla          ###   ########.fr       */
+/*   Updated: 2025/11/22 13:29:04 by ahbilla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,41 +17,14 @@
 class ScavTrap : public ClapTrap
 {
 public:
-    ScavTrap() : ClapTrap()
-    {
-        HitPoint= 100 ;
-        EnergyPoint =50;
-        AttackDamage = 20;
-        std::cout << Name << " ScavTrap constructor is called" << std::endl;
-    }
-    ScavTrap(std::string parm) : ClapTrap(parm) 
-    {
-        HitPoint= 100 ;
-        EnergyPoint =50;
-        AttackDamage = 20;
-        std::cout << Name << " ScavTrap parametrize constructor is called" << std::endl;
-    }
+    ScavTrap();
+    ScavTrap(std::string parm);
     void attack(const std::string &target);
-    void guardGate()
-    {
-        std::cout << Name << " is in Gate Keeper mode" << std::endl;
-    }
-    ScavTrap(const ScavTrap &other) : ClapTrap(other)
-    {
-        // *this = other;
-        std::cout << Name << " ScavTrap copy constructor called" << std::endl;
-    }
+    void guardGate();
+    ScavTrap(const ScavTrap &other);
+    ScavTrap &operator=(const ScavTrap &other);
+    ~ScavTrap();
 
-    ScavTrap &operator=(const ScavTrap &other)
-    {
-        (ClapTrap::operator=(other));
-        std::cout << Name << " ScavTrap copy assignment operator called" << std::endl;
-        return *this;
-    }
-
-    ~ScavTrap()
-    {
-    }
 };
 
 #endif
