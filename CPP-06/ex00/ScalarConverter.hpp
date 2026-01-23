f845fb701ebe08fb5b0ccdb6dc22ce2ea9e1cbe4
ahmed billa <ahmedbilla@ahmeds-MacBook-Air-2.local>
2026-01-23 02:38:46 +0100

    cpp 06

--- CPP-06/ex00/ScalarConverter.hpp
+++ CPP-06/ex00/ScalarConverter.hpp
@@ -16,31 +16,119 @@
 #include <cstdlib>
 class ScalarConverter
 {
-    private:
-        // ScalarConverter(){}
-        // ScalarConverter(const ScalarConverter &other){}
-        // ScalarConverter &operator=(const ScalarConverter &other){}
-        // ~ScalarConverter(){}
-    public:
-        static void convert(std::string param)
+private:
+    // ScalarConverter(){}
+    // ScalarConverter(const ScalarConverter &other){}
+    // ScalarConverter &operator=(const ScalarConverter &other){}
+    // ~ScalarConverter(){}
+public:
+    static int check_ischar(const std::string &param)
+    {
+        if (param.length() == 1 && !std::isdigit(param[0]))
+            return 1;
+        return 0;
+    }
+    static int check_isnumber(const std::string &number)
+    {
+        int i = 0;
+        int result;
+        if (number.empty())
+            return 0;
+
+        if (number[i] == '+' || number[i] == '-')
+            i++;
+
+        if (i == number.length())
+            return 0;
+
+        while (i < number.length())
         {
-            if((param.length() == 1) && (std::isdigit(param[0])!= 1))
-            {
-                if(std::isprint(param[0]))
-                {
-                    std::cout << "char: " << param << std::endl;
-                    std::cout << "int: " << static_cast<int>(param[0]) << std::endl;
-                }
-                else
-                    std::cout<< "char: Non displayable" <<std::endl;
-            }
-            
-            // else if (z)
-            // {
-                
-            // }
-            
+            if (!std::isdigit(number[i]))
+                return 0;
+            i++;
         }
+        result = std::atoi(number.c_str());
+        std::cout << "Result: " << result << std::endl;
+        if (result > INT_MAX || result < INT_MIN)
+            return 0;
+        return 1;
+    }
+    static int check_isfloat(const std::string &number)
+    {
+        int i = 0;
+        int dot_count = 0;
+
+        if (number.empty())
+            return 0;
+
+        if (number[i] == '+' || number[i] == '-')
+            i++;
+
+        if (i == number.length())
+            return 0;
+
+        while (i < number.length())
+        {
+            if (number[i] == '.')
+                dot_count++;
+            else if (!std::isdigit(number[i]))
+                return 0;
+            i++;
+        }
+        if (dot_count != 1)
+            return 0;
+        return 1;
+    }
+    static int check_isdouble(const std::string &number)
+    {
+        int i = 0;
+        int dot_count = 0;
+
+        if (number.empty())
+            return 0;
+
+        if (number[i] == '+' || number[i] == '-')
+            i++;
+
+        if (i == number.length())
+            return 0;
+
+        while (i < number.length())
+        {
+            if (number[i] == '.')
+                dot_count++;
+            else if (!std::isdigit(number[i]))
+                return 0;
+            i++;
+        }
+        if (dot_count != 1)
+            return 0;
+        return 1;
+    }
+    static void convert(std::string param)
+    {
+        // if ((param.length() == 1) && (std::isdigit(param[0]) != 1))
+        // {
+        //     if (std::isprint(param[0]))
+        //     {
+        //         std::cout << "char: " << "'" << param << "'" << std::endl;
+        //         std::cout << "int: " << static_cast<int>(param[0]) << std::endl;
+        //     }
+        //     else
+        //         std::cout << "char: Non displayable" << std::endl;
+        // }
+
+        // else if (check_isnumber(param))
+        // {
+
+        //     std::cout << "int: " << param << std::endl;
+        //     // std::cout
+        // }
+        std::cout << check_ischar(param) << std::endl;
+        std::cout << check_isnumber(param) << std::endl;
+        std::cout << check_isfloat(param) << std::endl;
+        std::cout << check_isdouble(param) << std::endl;
+    }
 };
 
 #endif
--- CPP-06/ex00/help.cpp
+++ CPP-06/ex00/help.cpp
@@ -1,37 +1,47 @@
-#include <iostream>
-#include <cstdlib>
+// #include <iostream>
+// #include <cstdlib>
 
-int check_isnumber(std::string number, int length)
-{
-    int i = 0;
-    if(number[i] == '-' || number[i] == '+')
-        ++i;
-    while (i < length)
-    {
-        std::cout << number[i] << std::endl;
-        
-        if (isdigit(number[i]) && number[i] != '.')
-        {
-            std::cout << "rah rir number \n";
-        }
-        else
-        {
-            std::cout << "la rah khasr\n";
-        }
-        i++;
-    }
+// int check_isnumber(const std::string& number)
+// {
+//     int i = 0;
+
+//     if (number.empty())
+//         return 0;
+
+//     if (number[i] == '+' || number[i] == '-')
+//         i++;
+
+//     if (i == number.length())
+//         return 0;
+
+//     while (i < number.length())
+//     {
+//         if (!isdigit(number[i]))
+//             return 0;
+//         i++;
+//     }
+
+//     return 1;
+// }
+// int main(int ac, char *av[])
+// {
+//     std::string param;
+//     int len;
+//     if (ac == 2)
+//     {
+//         param = av[1];
+//         check_isnumber(param);
+//         // std::cout << param << std::endl << len << std::endl;
+//     }
+// }
+
+#include <stdio.h>
+#include <float.h> // Required for DBL_MAX
+
+int main() {
+    // %e prints in scientific notation (e.g., 1.79e+308)
+    // %f would be too long to read easily
+    printf("The maximum value of double is: %e\n", DBL_MAX);
+    
     return 0;
-}
-int main(int ac, char *av[])
-{
-    std::string param;
-    int len;
-    if (ac == 2)
-    {
-        param = av[1];
-
-        len = param.length();
-        check_isnumber(param, len);
-        // std::cout << param << std::endl << len << std::endl;
-    }
 }
